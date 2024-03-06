7749c1b654f946750826f7b958151b82446e3578
carlostxrres <ctorresmoral@gmail.com>
2024-03-06 19:01:38 +0100

    show last-time-seen on banner

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Idealista - Marca los inmuebles vistos
+
 Este script de Tampermonkey marca automáticamente los inmuebles que ya has visitado en Idealista con un pequeño banner, mejorando tu experiencia de navegación al buscar propiedades.
 
 ## Pre-requisitos
+
 Antes de instalar este script, necesitas tener Tampermonkey instalado en tu navegador.
 
 ### Instalación de Tampermonkey
 
 1. **Google Chrome / Microsoft Edge / Brave / Vivaldi:**
+
    - Visita [Chrome Web Store - Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) y haz clic en "Añadir a Chrome".
 
 2. **Mozilla Firefox:**
+
    - Visita [Firefox Add-ons - Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) y haz clic en "Añadir a Firefox".
 
 3. **Safari:**
@@ -32,10 +36,11 @@ Para instalar este script, sigue estos pasos:
 Una vez instalado, el script comenzará a funcionar inmediatamente en el sitio web de Idealista, marcando los inmuebles que ya has visitado.
 
 ## To do
+
 - [x] Begin with an initial version in TamperMonkey
 - [ ] On UI seen-banner:
-   - [ ] Show when the property was seen for the last time
-   - [ ] Add an option to "set as unseen" on property nodes (on click?)
+  - [x] Show when the property was seen for the last time
+  - [ ] Add an option to "set as unseen" on property nodes (on click?)
 - [ ] Allow real-time flagging
 - [ ] Migrate this to a Firefox add-on
 - [ ] Add "reset storage" option
--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Idealista - marca los inmuebles vistos
 // @name:es         Idealista - marca los inmuebles vistos
-// @version         0.0.1
+// @version         0.1.0
 // @description     En Idealista, marca los inmuebles vistos con un pequeño banner
 // @description:es  En Idealista, marca los inmuebles vistos con un pequeño banner
 // @author          carlostxrres
@@ -14,108 +14,157 @@
 // @grant           GM.addStyle
 // ==/UserScript==
 
-(function() {
-    'use strict'
+;(function () {
+  "use strict"
+  /* Settings */
+  const USER_SETTINGS = {
+    LANGUAGE: "es", // es en
+  }
 
-    /* Global constants */
-    const KEYS = {
-        SEEN_LIST: "seenList"
+  /* Global constants */
+  const KEYS = {
+    SEEN_LIST: "seenList",
+  }
+  const CLASSNAMES = {
+    SEEN_LISTED_INMUEBLE: "seen-listed-inmueble",
+    SEEN_LISTED_INMUEBLE_BANNER: "seen-listed-inmueble-banner",
+  }
+
+  const timeUnits = {
+    hour: 1000 * 60 * 60,
+    day: 1000 * 60 * 60 * 24,
+    week: 1000 * 60 * 60 * 24 * 7,
+    month: 1000 * 60 * 60 * 24 * 7 * 30,
+    year: 1000 * 60 * 60 * 24 * 7 * 30 * 365.25,
+  }
+  const relativeTimeFormat = new Intl.RelativeTimeFormat(
+    USER_SETTINGS.LANGUAGE,
+    { numeric: "auto" }
+  )
+
+  const translations = {
+    en: {
+      Seen: "Seen",
+    },
+    es: {
+      Seen: "Visto",
+    },
+  }
+  const translate = (key) => translations[USER_SETTINGS.LANGUAGE][key]
+
+  /* Script */
+  startUp()
+
+  function startUp() {
+    addCss()
+
+    const paths = location.pathname.split("/").filter(Boolean)
+
+    const actionByPath = {
+      inmueble: handleInmueblePage,
+      "alquiler-viviendas": handleInmuebleListPage,
+      "venta-viviendas": handleInmuebleListPage,
     }
-    const CLASSNAMES = {
-        SEEN_LISTED_INMUEBLE: "seen-listed-inmueble",
-        SEEN_LISTED_INMUEBLE_BANNER: "seen-listed-inmueble-banner",
+    actionByPath[paths[0]]?.(paths)
+  }
+
+  async function handleInmueblePage([, id]) {
+    const seenInmuebles = await GM.getValue(KEYS.SEEN_LIST, {})
+    seenInmuebles[id] = {
+      lastSeen: new Date().getTime(),
     }
+    await GM.setValue(KEYS.SEEN_LIST, seenInmuebles)
+  }
 
-    /* Script */
-    startUp()
+  async function handleInmuebleListPage() {
+    const seenInmuebles = await GM.getValue(KEYS.SEEN_LIST, {})
+    const inmuebleNodes = Array.from(
+      document.querySelectorAll(".items-container.items-list > article.item")
+    )
+    inmuebleNodes.forEach((inmuebleNode) => {
+      const inmuebleNodeId = inmuebleNode.dataset.adid
+      const lastSeenInfo = seenInmuebles[inmuebleNodeId]
+      if (!lastSeenInfo) return
 
-    function startUp() {
-        addCss()
+      displayInmuebleNodeAsSeen(inmuebleNode, lastSeenInfo)
+    })
+  }
 
-        const paths = location
-        .pathname
-        .split("/")
-        .filter(Boolean)
+  function displayInmuebleNodeAsSeen(inmuebleNode, lastSeenInfo) {
+    inmuebleNode.classList.add(CLASSNAMES.SEEN_LISTED_INMUEBLE)
 
-        const actionByPath = {
-            "inmueble": handleInmueblePage,
-            "alquiler-viviendas": handleInmuebleListPage,
-            "venta-viviendas": handleInmuebleListPage,
-        }
-        actionByPath[paths[0]]?.(paths)
+    const banner = document.createElement("div")
+    banner.innerHTML = `${getIconSvg("eye")} ${translate(
+      "Seen"
+    )} ${getRelativePastTime(lastSeenInfo.lastSeen)}` // getIconSvg("eye") + "Seen"
+    banner.className = CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER
+    inmuebleNode.appendChild(banner)
+  }
+
+  function getRelativePastTime(timestamp) {
+    const timeUnitEntries = Object.entries(timeUnits)
+    const elapsed = new Date() - new Date(timestamp)
+
+    /**
+     * @var unitIndex and adjustedUnitIndex:
+     * Identify the first entry where the elapsed time is shorter than the time unit's millisecond
+     * value, or fall back to the greatest unit if no such entry exists. Then, adjust to one smaller
+     * unit to enrich the information conveyed (e.g., "three days ago" rather than "this week").
+     * Unless the unit determined is the smallest available, to avoid an invalid index of -1.
+     */
+    const unitIndex = timeUnitEntries.findIndex(
+      ([, unitMilliseconds], i) =>
+        elapsed < unitMilliseconds || i === timeUnits.length - 1
+    )
+    const adjustedUnitIndex = Math.max(unitIndex - 1, 0)
+
+    const [unit, unitMilliseconds] = timeUnitEntries[adjustedUnitIndex]
+    const elapsedUnits = Math.floor(elapsed / unitMilliseconds)
+    return relativeTimeFormat.format(-Math.abs(elapsedUnits), unit)
+  }
+
+  function getIconSvg(icon, { size = 24, strokeWidth = 1.5 } = {}) {
+    // Icons from https://tabler.io/icons
+    const svgContent = {
+      eye: `<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />`,
     }
+    return [
+      `<svg`,
+      ` xmlns="http://www.w3.org/2000/svg"`,
+      ` width="${size}"`,
+      ` height="${size}"`,
+      ` viewBox="0 0 ${size} ${size}"`,
+      ` stroke-width="${strokeWidth}"`,
+      ` stroke="currentColor"`,
+      ` fill="none"`,
+      ` stroke-linecap="round"`,
+      ` stroke-linejoin="round"`,
+      `>${svgContent[icon]}</svg>`,
+    ].join("")
+  }
 
-    async function handleInmueblePage([, id]) {
-        const seenInmuebles = await GM.getValue(KEYS.SEEN_LIST, {})
-        seenInmuebles[id] = {
-            lastSeen: new Date().getTime()
-        }
-        await GM.setValue(KEYS.SEEN_LIST, seenInmuebles)
-    }
+  function addCss() {
+    const css = [
+      `.${CLASSNAMES.SEEN_LISTED_INMUEBLE} {`,
+      `  position: relative;`,
+      `}`,
 
-    async function handleInmuebleListPage() {
-        const seenInmuebles = await GM.getValue(KEYS.SEEN_LIST, {})
-        const inmuebleNodes = Array.from(document.querySelectorAll(".items-container.items-list > article.item"))
-        inmuebleNodes.forEach(inmuebleNode => {
-            const inmuebleNodeId = inmuebleNode.dataset.adid
-            const isSeen = Object.hasOwn(seenInmuebles, inmuebleNodeId)
-            if (!isSeen) return
+      `.${CLASSNAMES.SEEN_LISTED_INMUEBLE} > *:not(.${CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER}) {`,
+      `  filter: opacity(.4);`,
+      `}`,
 
-            displayInmuebleNodeAsSeen(inmuebleNode)
-        })
-    }
-
-    function displayInmuebleNodeAsSeen(inmuebleNode) {
-        inmuebleNode.classList.add(CLASSNAMES.SEEN_LISTED_INMUEBLE)
-
-        const banner = document.createElement("div")
-        banner.innerHTML = getIconSvg("eye") + "Seen"
-        banner.className = CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER
-        inmuebleNode.appendChild(banner)
-    }
-
-    function getIconSvg(icon, { size = 24, strokeWidth = 1.5 } = {}) {
-        // Icons from https://tabler.io/icons
-        const svgContent = {
-            eye: `<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />`,
-        }
-        return [
-            `<svg`,
-            ` xmlns="http://www.w3.org/2000/svg"`,
-            ` width="${size}"`,
-            ` height="${size}"`,
-            ` viewBox="0 0 ${size} ${size}"`,
-            ` stroke-width="${strokeWidth}"`,
-            ` stroke="currentColor"`,
-            ` fill="none"`,
-            ` stroke-linecap="round"`,
-            ` stroke-linejoin="round"`,
-            `>${svgContent[icon]}</svg>`,
-        ].join("")
-    }
-
-    function addCss() {
-        const css = [
-            `.${CLASSNAMES.SEEN_LISTED_INMUEBLE} {`,
-            `  position: relative;`,
-            `}`,
-
-            `.${CLASSNAMES.SEEN_LISTED_INMUEBLE} > *:not(.${CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER}) {`,
-            `  filter: opacity(.4);`,
-            `}`,
-
-            `.${CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER} {`,
-            `  position: absolute;`,
-            `  inset: 1rem auto auto 1rem;`,
-            `  background-color: #0009;`,
-            `  color: white;`,
-            `  padding: .5rem 1rem;`,
-            `  display: flex;`,
-            `  gap: .5rem;`,
-            `  border-radius: 4px;`,
-            `  border: 1px solid #f4f5f2;`,
-            `}`,
-        ].join("\n")
-        GM.addStyle(css)
-    }
+      `.${CLASSNAMES.SEEN_LISTED_INMUEBLE_BANNER} {`,
+      `  position: absolute;`,
+      `  inset: 1rem auto auto 1rem;`,
+      `  background-color: #0009;`,
+      `  color: white;`,
+      `  padding: .5rem 1rem;`,
+      `  display: flex;`,
+      `  gap: .5rem;`,
+      `  border-radius: 4px;`,
+      `  border: 1px solid #f4f5f2;`,
+      `}`,
+    ].join("\n")
+    GM.addStyle(css)
+  }
 })()
